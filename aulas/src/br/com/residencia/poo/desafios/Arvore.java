0d73ff8d347479779387a714436b64c663148af5
Moacyr kennedy <105392455+MoacyrKennedy@users.noreply.github.com>
2022-09-14 15:38:53 -0300

    Initial commit

---
+++ aulas/src/br/com/residencia/poo/desafios/Arvore.java
@@ -0,0 +1,28 @@
+package br.com.residencia.poo.desafios;
+
+public class Arvore {
+
+	public static void main(String[] args) {
+
+		int linha = 8, t = 1;
+
+		for (int cont = 1; cont <= 8; cont++) {
+			for (int contDois = 1; contDois <= 10; contDois++)
+				if (contDois == linha) {
+					for (int contTres = 1; contTres <= t; contTres++)
+						System.out.print('*');
+
+					t = t + 2;
+					linha--;
+
+				} else {
+					System.out.print(' ');
+
+				}
+			System.out.println();
+		}
+		System.out.println("      |_|");
+		System.out.println();
+		System.out.println("Árvore Serratec");
+	}
+}
---
+++ aulas/src/br/com/residencia/poo/primeiraaula/PrimeiroExercicio.java
@@ -0,0 +1,10 @@
+package br.com.residencia.poo.primeiraaula;
+
+public class PrimeiroExercicio {
+
+	public static void main(String[] args) {
+		System.out.println("Olá mundo!");
+	}
+
+}
+
